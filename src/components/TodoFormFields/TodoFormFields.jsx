0ba50aa2c92cb115e75938bd2479ed67141153a1
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-01-26 21:50:11 +0530

    error messages

--- src/components/TodoFormFields/TodoFormFields.jsx
+++ src/components/TodoFormFields/TodoFormFields.jsx
@@ -1,7 +1,12 @@
 import { PRIORITIES, PRIORITY_DEFAULT } from "../../constants/priorities";
 import styles from "./TodoFormFields.module.css";
 
-export function TodoFormFields({ todo = {}, showAllFields = true, register }) {
+export function TodoFormFields({
+  todo = {},
+  showAllFields = true,
+  register,
+  errors = {},
+}) {
   return (
     <div className={styles.FormFields}>
       <div className={styles.FormField}>
@@ -11,8 +16,22 @@ export function TodoFormFields({ todo = {}, showAllFields = true, register }) {
           placeholder="Name*"
           autoComplete="off"
           defaultValue={todo.name}
-          {...register("name", { required: true, minLength: 3, maxLength: 50 })}
+          aria-invalid={!!errors.name}
+          {...register("name", {
+            required: "Name is required",
+            minLength: {
+              value: 3,
+              message: "Name should have min length of 3 characters",
+            },
+            maxLength: {
+              value: 50,
+              message: "Name should have max length of 50 characters",
+            },
+          })}
         />
+        {!!errors.name && (
+          <span className={styles.FormFieldError}>{errors.name.message}</span>
+        )}
       </div>
 
       {showAllFields && (
@@ -23,8 +42,20 @@ export function TodoFormFields({ todo = {}, showAllFields = true, register }) {
               placeholder="Description"
               rows="3"
               defaultValue={todo.description}
-              {...register("description", { maxLength: 200 })}
+              aria-invalid={!!errors.description}
+              {...register("description", {
+                maxLength: {
+                  value: 200,
+                  message:
+                    "Description should have max length of 200 characters",
+                },
+              })}
             />
+            {!!errors.description && (
+              <span className={styles.FormFieldError}>
+                {errors.description.message}
+              </span>
+            )}
           </div>
 
           <div className={styles.FormGroup}>
@@ -34,13 +65,19 @@ export function TodoFormFields({ todo = {}, showAllFields = true, register }) {
                 type="date"
                 id="deadline"
                 defaultValue={todo.deadline}
-                {...register(
-                  "deadline",
-                  !todo.id && {
-                    min: new Date().toISOString().split("T")[0],
-                  }
-                )}
+                aria-invalid={!!errors.deadline}
+                {...register("deadline", {
+                  min: !todo.id && {
+                    value: new Date().toISOString().split("T")[0],
+                    message: "Deadline can't be date in past",
+                  },
+                })}
               />
+              {!!errors.deadline && (
+                <span className={styles.FormFieldError}>
+                  {errors.deadline.message}
+                </span>
+              )}
             </div>
 
             <div className={styles.FormField}>
@@ -48,7 +85,12 @@ export function TodoFormFields({ todo = {}, showAllFields = true, register }) {
               <select
                 defaultValue={todo.priority ?? PRIORITY_DEFAULT}
                 id="priority"
-                {...register("priority")}
+                aria-invalid={!!errors.priority}
+                {...register("priority", {
+                  validate: (value) =>
+                    Object.keys(PRIORITIES).includes(value) ||
+                    "Priority is not valid value",
+                })}
               >
                 {Object.entries(PRIORITIES).map(([key, { label }]) => (
                   <option key={key} value={key}>
@@ -56,6 +98,11 @@ export function TodoFormFields({ todo = {}, showAllFields = true, register }) {
                   </option>
                 ))}
               </select>
+              {!!errors.priority && (
+                <span className={styles.FormFieldError}>
+                  {errors.priority.message}
+                </span>
+              )}
             </div>
           </div>
         </>
