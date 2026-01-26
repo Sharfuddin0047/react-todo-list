51763aea49a95034400bd65c95ba298c74bda76b
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-01-26 17:57:37 +0530

    edit features

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,39 +1,79 @@
+import { useState } from "react";
 import { PRIORITIES, PRIORITY_DEFAULT } from "../../constants/priorities";
+import { TodoFormFields } from "../TodoFormFields/TodoFormFields";
 import styles from "./TodoListItem.module.css";
 
 export function TodoListItem({ todo, onUpdate }) {
+  const [isEditing, setIsEditing] = useState(false);
+
   function handleCompleted(event) {
     onUpdate(todo.id, { ...todo, completed: event.target.checked });
   }
 
-  return (
-    <li className={styles.TodoListItem} data-completed={todo.completed}>
-      <div className={styles.Content}>
-        <input
-          type="checkbox"
-          name="completed"
-          checked={todo.completed}
-          onChange={handleCompleted}
-          className={styles.Status}
-        />
+  function handleEdit(event) {
+    event.preventDefault();
 
-        <div className={styles.Info}>
-          {todo.name}
+    const { elements } = event.target;
+    if (elements.name.value === "") return;
 
-          {todo.description && (
-            <span className={styles.Description}>{todo.description}</span>
+    onUpdate(todo.id, {
+      name: elements.name.value,
+      description: elements.description.value,
+      deadline: elements.deadline.value,
+      priority: elements.priority.value,
+      completed: todo.completed,
+    });
+
+    setIsEditing(false);
+  }
+
+  const viewingTemplate = (
+    <div className={styles.Content}>
+      <input
+        type="checkbox"
+        name="completed"
+        checked={todo.completed}
+        onChange={handleCompleted}
+        className={styles.Status}
+      />
+
+      <div className={styles.Info}>
+        {todo.name}
+
+        {todo.description && (
+          <span className={styles.Description}>{todo.description}</span>
+        )}
+
+        <div className={styles.AdditionalInfo}>
+          {todo.deadline}{" "}
+          {todo.priority !== PRIORITY_DEFAULT && (
+            <span style={{ color: PRIORITIES[todo.priority].color }}>
+              {PRIORITIES[todo.priority].label}
+            </span>
           )}
-
-          <div className={styles.AdditionalInfo}>
-            {todo.deadline}{" "}
-            {todo.priority !== PRIORITY_DEFAULT && (
-              <span style={{ color: PRIORITIES[todo.priority].color }}>
-                {PRIORITIES[todo.priority].label}
-              </span>
-            )}
-          </div>
         </div>
       </div>
+
+      <div className={styles.Controls}>
+        <button onClick={() => setIsEditing(true)}>📝</button>
+      </div>
+    </div>
+  );
+
+  const editingTemplate = (
+    <form className={styles.Content} onReset={() => setIsEditing(false)} onSubmit={handleEdit}>
+      <TodoFormFields todo={todo} />
+
+      <div className={styles.Controls}>
+        <input type="submit" value="💾" />
+        <input type="reset" value="❌" />
+      </div>
+    </form>
+  );
+
+  return (
+    <li className={styles.TodoListItem} data-completed={todo.completed}>
+      {isEditing ? editingTemplate : viewingTemplate}
     </li>
   );
 }
